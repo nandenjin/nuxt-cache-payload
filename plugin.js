fc47a68d63ad2ff0c733101af47cc8a51e9032be
Kazumi Inada <timer.ground.gm@gmail.com>
2019-02-25 00:01:18 +0900

    Fix error 'isStatic is not defined'

--- plugin.js
+++ plugin.js
@@ -11,36 +11,6 @@ import consola from 'consola';
 
 const filename = '<%= options.filename %>';
 
-/**
- * Retrive cached payload data.
- * @param {String} path Page path for payload data to retrive. (Ex: context.route.path)
- * @return {Promise<object>} Promise contains retrived payload object (JSON serialized). Resolved with null when it is called in non-generated environment or failed to fetch.
- */
-async function getPayload( path ) {
-
-  // Retrive path to payload
-  const filePath = joinPath( path, filename );
-
-  // is on generated site && is not in process of generate
-  if( isStatic && process.client ) {
-
-    try {
-
-      return ( await axios.get( filePath ) ).data;
-
-    }catch( e ) {
-
-      return null;
-
-    }
-
-  }else {
-
-    return null;
-
-  }
-
-}
 
 // Helper method to join path fragments
 function joinPath( ...fragments ) {
@@ -53,6 +23,35 @@ export default ( context, inject ) => {
 
   const isStatic = context.isStatic;
 
-  context.getPayload = getPayload;
+  /**
+   * Retrive cached payload data.
+   * @param {String} path Page path for payload data to retrive. (Ex: context.route.path)
+   * @return {Promise<object>} Promise contains retrived payload object (JSON serialized). Resolved with null when it is called in non-generated environment or failed to fetch.
+   */
+  context.getPayload = async function getPayload( path ) {
+
+    // Retrive path to payload
+    const filePath = joinPath( path, filename );
+
+    // is on generated site && is not in process of generate
+    if( isStatic && process.client ) {
+
+      try {
+
+        return ( await axios.get( filePath ) ).data;
+
+      }catch( e ) {
+
+        return null;
+
+      }
+
+    }else {
+
+      return null;
+
+    }
+
+  };
 
 };
