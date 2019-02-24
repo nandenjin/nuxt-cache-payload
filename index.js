1e713b989c306cb433db2aef16a56aad80a0d99d
Kazumi Inada <timer.ground.gm@gmail.com>
2019-02-24 23:17:36 +0900

    Refactor

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const consola = require( 'consola' );
 
 module.exports = function cachePayload( options = {} ) {
 
-
   const nuxt = this.nuxt;
   const nuxtOptions = this.options;
 
@@ -35,39 +34,7 @@ module.exports = function cachePayload( options = {} ) {
 
 
   // Hook for end of generate
-  nuxt.hook( 'generate:done', () => {
-
-    consola.info( 'nuxt-cache-payload is writing payload cache files...' );
-    consola.info( `Payload filename: ${ filename }` );
-
-    for( let i = 0; i < routes.length; i++ ) {
-
-      const route = routes[ i ].route;
-      const payload = routes[ i ].payload;
-
-      // Pass if payload does not be provided
-      if( payload === null ) continue;
-
-      // Write to distDir
-      fs.writeFile(
-        path.join( distPath, route, filename ),
-        JSON.stringify( payload ),
-        encoding,
-        err => {
-
-          if( !err ) consola.success( 'Generated payload cache for ' + route );
-
-          else {
-            consola.error( 'Failed to create payload file' );
-            console.dir( err );
-          }
-
-        }
-      );
-
-    }
-
-  } );
+  nuxt.hook( 'generate:done', () => generateCacheFiles( routes, { distPath, filename, encoding } ) );
 
 
   // Register plugin to provide method to retrive data
@@ -81,5 +48,42 @@ module.exports = function cachePayload( options = {} ) {
 
 };
 
+
 // Metadata
 module.exports.meta = require( './package.json' );
+
+
+// Generate cache files from routes
+function generateCacheFiles( routes, { distPath, filename, encoding } ) {
+
+  consola.info( 'nuxt-cache-payload is writing payload cache files...' );
+  consola.info( `Payload filename: ${ filename }` );
+
+  for( let i = 0; i < routes.length; i++ ) {
+
+    const route = routes[ i ].route;
+    const payload = routes[ i ].payload;
+
+    // Pass if payload does not be provided
+    if( payload === null ) continue;
+
+    // Write to distDir
+    fs.writeFile(
+      path.join( distPath, route, filename ),
+      JSON.stringify( payload ),
+      encoding,
+      err => {
+
+        if( !err ) consola.success( 'Generated payload cache for ' + route );
+
+        else {
+          consola.error( 'Failed to create payload file' );
+          console.dir( err );
+        }
+
+      }
+    );
+
+  }
+
+}
