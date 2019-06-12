3580bb76fb785e73d72586a97f1549ae28cc20ac
Kazumi Inada <hello@nandenjin.com>
2019-06-12 09:45:23 +0900

    Auto lintfix

--- index.js
+++ index.js
@@ -5,85 +5,70 @@
 
 */
 
-const path = require( 'path' );
-const fs = require( 'fs' );
+const path = require('path')
+const fs = require('fs')
 
-const consola = require( 'consola' );
+const consola = require('consola')
 
-
-module.exports = function cachePayload( options = {} ) {
-
-  const nuxt = this.nuxt;
-  const nuxtOptions = this.options;
+module.exports = function cachePayload(options = {}) {
+  const nuxt = this.nuxt
+  const nuxtOptions = this.options
 
   // Path for distDir
-  const distPath = nuxtOptions.generate.dir;
+  const distPath = nuxtOptions.generate.dir
 
   // Generate random hash
-  const hash = Math.floor( Math.random() * 1000000 );
+  const hash = Math.floor(Math.random() * 1000000)
 
   // Loading options
-  const filename = options.filename || `_payload.${ hash }.json`;
-  const encoding = options.encoding || 'utf8';
+  const filename = options.filename || `_payload.${hash}.json`
+  const encoding = options.encoding || 'utf8'
 
-  let routes;
-
+  let routes
 
   // Hook for generated routes
-  nuxt.hook( 'generate:extendRoutes', r => routes = [ ...r ] );
-
+  nuxt.hook('generate:extendRoutes', r => routes = [ ...r ])
 
   // Hook for end of generate
-  nuxt.hook( 'generate:done', () => generateCacheFiles( routes, { distPath, filename, encoding } ) );
-
+  nuxt.hook('generate:done', () => generateCacheFiles(routes, { distPath, filename, encoding }))
 
   // Register plugin to provide method to retrive data
-  this.addPlugin( {
-    src: path.resolve( __dirname, 'plugin.js' ),
+  this.addPlugin({
+    src: path.resolve(__dirname, 'plugin.js'),
     options: {
-      filename,
-    },
-  } );
-
-
-};
-
+      filename
+    }
+  })
+}
 
 // Metadata
-module.exports.meta = require( './package.json' );
-
+module.exports.meta = require('./package.json')
 
 // Generate cache files from routes
-function generateCacheFiles( routes, { distPath, filename, encoding } ) {
+function generateCacheFiles(routes, { distPath, filename, encoding }) {
+  consola.info('nuxt-cache-payload is writing payload cache files...')
+  consola.info(`Payload filename: ${filename}`)
 
-  consola.info( 'nuxt-cache-payload is writing payload cache files...' );
-  consola.info( `Payload filename: ${ filename }` );
-
-  for( let i = 0; i < routes.length; i++ ) {
-
-    const route = routes[ i ].route;
-    const payload = routes[ i ].payload;
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[ i ].route
+    const payload = routes[ i ].payload
 
     // Pass if payload does not be provided
-    if( payload === null ) continue;
+    if (payload === null) continue
 
     // Write to distDir
     fs.writeFile(
-      path.join( distPath, route, filename ),
-      JSON.stringify( payload ),
+      path.join(distPath, route, filename),
+      JSON.stringify(payload),
       encoding,
-      err => {
-
-        if( !err ) consola.success( 'Generated payload cache for ' + route );
+      (err) => {
+        if (!err) consola.success('Generated payload cache for ' + route)
 
         else {
-          consola.error( 'Failed to create payload file' );
-          console.dir( err );
+          consola.error('Failed to create payload file')
+          console.dir(err)
         }
-
       }
-    );
-
+    )
   }
-
 }
--- plugin.js
+++ plugin.js
@@ -5,53 +5,37 @@
 
 */
 
+import axios from 'axios'
+import consola from 'consola'
 
-import axios from 'axios';
-import consola from 'consola';
-
-const filename = '<%= options.filename %>';
-
+const filename = '<%= options.filename %>'
 
 // Helper method to join path fragments
-function joinPath( ...fragments ) {
-
-  return fragments.join( '/' ).replace( /\/{1,}/g, '/' );
-
+function joinPath(...fragments) {
+  return fragments.join('/').replace(/\/{1,}/g, '/')
 }
 
-export default ( context, inject ) => {
-
-  const isStatic = context.isStatic;
+export default (context, inject) => {
+  const isStatic = context.isStatic
 
   /**
    * Retrive cached payload data.
    * @param {String} path Page path for payload data to retrive. (Ex: context.route.path)
    * @return {Promise<object>} Promise contains retrived payload object (JSON serialized). Resolved with null when it is called in non-generated environment or failed to fetch.
    */
-  context.getPayload = async function getPayload( path ) {
-
+  context.getPayload = async function getPayload(path) {
     // Retrive path to payload
-    const filePath = joinPath( path, filename );
+    const filePath = joinPath(path, filename)
 
     // is on generated site && is not in process of generate
-    if( isStatic && process.client ) {
-
+    if (isStatic && process.client) {
       try {
-
-        return ( await axios.get( filePath ) ).data;
-
-      }catch( e ) {
-
-        return null;
-
+        return (await axios.get(filePath)).data
+      } catch (e) {
+        return null
       }
-
-    }else {
-
-      return null;
-
+    } else {
+      return null
     }
-
-  };
-
-};
+  }
+}
